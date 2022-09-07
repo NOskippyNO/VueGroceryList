d033c8edbfa7d91efe5cef1c7d702f0ace77408a
Arnoldo Cantu <arnoldo.cantu17@yahoo.com>
2022-09-06 22:54:50 -0500

    Forgot to Commit throughout the code.

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<script setup>
+  import {ref} from 'vue'
+  import { useStorage } from '@vueuse/core'
+  import { nanoid } from 'nanoid'
+  import confetti from 'canvas-confetti'
+  
+
+  const newGrocery = ref('')
+  const groceries = useStorage('groceries', [])
+
+  const addGrocery = () => {
+    if(newGrocery.value) {
+      groceries.value.push({id: nanoid(), name: newGrocery.value })
+      newGrocery.value = ''
+    }
+  }
+
+  const deleteGrocery = id => {
+    const removeIndex = groceries.value.findIndex(grocery => grocery.id == id)
+    groceries.value.splice(removeIndex, 1)
+    confetti({ particleCount: 300, spread: 1000, origin: { y: 1 } })
+  }
+</script>
+
+<template>
+  <main>
+    <h1 class="title">✏️📑 Vue Grocery List 📑✏️</h1>
+
+    <form class="newGroceryForm" @submit.prevent="addGrocery">
+      <input 
+        id="newGrocery" 
+        autocomplete="off" 
+        type="text" 
+        placeholder="Add an item to your list" 
+        v-model="newGrocery"
+      />
+      <button type="submit">Add</button>
+    </form>
+    <h3>Pending Grocery Count: {{groceries.length }}</h3>
+    <ul>
+      <li v-for="grocery in groceries" @click="deleteGrocery(grocery.id)">
+        {{ grocery.name }}
+      </li>
+    </ul>
+  </main>
+</template>
+
+<style lang="postcss" scoped>
+  main {
+    @apply mt-8 flex flex-col justify-center items-center gap-5;
+
+    .title {
+    @apply m-2 text-5xl font-light tracking-wider text-purplish;
+    }
+
+    form{
+      @apply flex focus-within:ring-8 focus-within:ring-accent focus-within:rounded-lg;
+      
+      input{
+        @apply bg-white text-comment p-2 w-80 text-2xl rounded-l-md outline-none;
+      }
+    }
+    button{
+      @apply bg-accent text-background p-2 text-2xl font-bold rounded-r-md;
+      &:hover {
+        @apply bg-yellowish;
+      }
+    }
+    ul{
+      @apply flex flex-col items-center justify-center rounded-lg bg-comment;
+      li{
+        @apply bg-white text-background m-3 p-2 w-96 text-center;
+        &:hover {
+          @apply bg-redish font-bold cursor-pointer;
+        }
+      }
+    }
+  }
+</style>
